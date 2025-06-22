7d1bb30e615ca6ea0955a0aca23e70e57286b093
David Andrs <andrsd@gmail.com>
2025-06-22 08:46:52 -0600

    Adding CVSDiff checker
    Needs testing

--- kuristo/actions/__init__.py
+++ kuristo/actions/__init__.py
@@ -1,12 +1,14 @@
 from .mpi_action import MPIAction
 from .seq_action import SeqAction
 from .checks_exodiff import ExodiffCheck
+from .checks_cvsdiff import CSVDiffCheck
 from .checks_regex import RegexCheck
 from .checks_regex_float import RegexFloatCheck
 
 
 __all__ = [
     "ExodiffCheck",
+    "CSVDiffCheck",
     "MPIAction",
     "SeqAction",
     "RegexCheck",
