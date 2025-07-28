a754ce303f690dcf503ed8bc78fda23cc833c48f
David Andrs <andrsd@gmail.com>
2025-07-28 16:40:15 -0600

    Adding placeholders for complicated regexs
    - {:float:} to match a floating point number
    - {:int:} to match an integer
    
    Closes #5

--- kuristo/actions/checks_regex.py
+++ kuristo/actions/checks_regex.py
@@ -3,13 +3,21 @@ from kuristo.registry import action
 from kuristo.actions.action import Action
 from kuristo.utils import interpolate_str
 
+ALIAS_PATTERNS = {
+    "float": r"([-+]?(?:\d*\.\d+|\d+)(?:[eE][-+]?\d+)?)",
+    "int": r"([-+]?\d+)",
+}
+
+ALIAS_RE = re.compile(r"{:(\w+):}")
+
 
 @action("checks/regex")
 class RegexCheck(Action):
+
     def __init__(self, name, context, **kwargs):
         super().__init__(name, context, **kwargs)
         self._target_step = kwargs["input"]
-        self._pattern = kwargs.get("pattern", [])
+        self._pattern = self._expand_pattern(kwargs.get("pattern", []))
 
     def run(self, context=None):
         output = self._resolve_output()
@@ -24,3 +32,11 @@ class RegexCheck(Action):
 
     def _resolve_output(self):
         return interpolate_str(self._target_step, self.context.vars)
+
+    def _expand_pattern(self, pattern: str) -> str:
+        def replacer(match):
+            name = match.group(1)
+            # fallback to original if not found
+            return ALIAS_PATTERNS.get(name, match.group(0))
+
+        return ALIAS_RE.sub(replacer, pattern)
