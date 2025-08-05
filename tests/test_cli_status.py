e151f5a722ce83224277ebfaf898ef32a148639b
David Andrs <andrsd@gmail.com>
2025-08-05 16:29:03 -0600

    Adding test for filtering the status command

--- tests/test_cli_status.py
+++ tests/test_cli_status.py
@@ -48,6 +48,38 @@ def test_print_report_outputs_correctly(
     mock_RunStats.assert_called_once_with(1, 1, 0)
 
 
+@patch("kuristo.cli._status.ui.status_line")
+@patch("kuristo.cli._status.ui.line")
+@patch("kuristo.cli._status.ui.stats")
+@patch("kuristo.cli._status.ui.time")
+@patch("kuristo.cli._status.ui.RunStats")
+@patch("kuristo.cli._status.config.get")
+def test_print_report_outputs_filtered(
+    mock_config_get, mock_RunStats, mock_time, mock_stats, mock_line, mock_status_line
+):
+    report = {
+        "results": [
+            {"status": "success", "job name": "Test A"},
+            {"status": "success", "job name": "Test B"},
+            {"status": "failed", "job name": "Another"},
+        ],
+        "total_runtime": 12.34
+    }
+
+    mock_cfg = MagicMock()
+    mock_cfg.console_width = 20
+    mock_config_get.return_value = mock_cfg
+
+    print_report(report, ["failed"])
+
+    # Check status_line called per entry
+    assert mock_status_line.call_count == 1
+    mock_line.assert_called_once()
+    mock_stats.assert_called_once()
+    mock_time.assert_called_once_with(12.34)
+    mock_RunStats.assert_called_once_with(0, 1, 0)
+
+
 @patch("kuristo.cli._status.print_report")
 @patch("kuristo.cli._status.utils.read_report")
 @patch("kuristo.cli._status.config.get")
