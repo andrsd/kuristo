4f898fa1a01f78e9a9c15ec44f70a382ae3cd947
David Andrs <andrsd@gmail.com>
2025-11-13 18:30:41 -0700

    Adding `working-directory` to job
    We can specify `working-directory` for a job "globally", so we don't
    have to specify it for each individual step.

--- kuristo/context.py
+++ kuristo/context.py
@@ -6,8 +6,9 @@ class Context:
     Context that "tags along" when excuting steps
     """
 
-    def __init__(self, base_env=None, matrix=None):
+    def __init__(self, base_env=None, defaults=None, matrix=None):
         self.env = Env(base_env)
+        self.defaults = defaults
         # variables for substitution
         self.vars = {
             "matrix": matrix,
