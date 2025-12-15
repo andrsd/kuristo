c6681f84f814cce806ee73a7b199a7331bf29445
David Andrs <andrsd@gmail.com>
2025-12-15 16:15:31 -0700

    Adding simple unit test for `build_filters`

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,7 @@
 import pytest
+from unittest.mock import MagicMock
 from kuristo.utils import interpolate_str, minutes_to_hhmmss, human_time
+from kuristo.utils import build_filters
 
 
 def test_interpolate_str_vars():
@@ -36,3 +38,11 @@ def test_human_time():
     assert human_time(1.06) == "1.06s"
     assert human_time(61.2) == "1m 1.20s"
     assert human_time(3765.2) == "1h 2m 45.20s"
+
+
+def test_build_filters():
+    args = MagicMock()
+    args.passed = True
+    args.skipped = True
+    args.failed = True
+    assert build_filters(args) == ["failed", "skipped", "success"]
